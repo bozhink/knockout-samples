e903b9ea1974634a1a2887d678b0e7ec7999f0af
Bozhin Karaivanov <bozhink@gmail.com>
2016-09-11 21:53:00 +0300

    SPA: WebmailViewModel with AJAX is implemeted.

--- single-page-application/app/view-models/webmail-view-model.js
+++ single-page-application/app/view-models/webmail-view-model.js
@@ -1,14 +1,35 @@
 function WebmailViewModel() {
-    var self = this;
-
     // Data
+    var self = this;
     self.folders = ['Inbox', 'Archive', 'Sent', 'Spam'];
     self.chosenFolderId = ko.observable();
+    self.chosenFolderData = ko.observable();
+    self.chosenMailData = ko.observable();
 
     // Behaviours
-    self.goToFolder = function(folder) {
-        if (folder) {
-            self.chosenFolderId(folder);
-        }
-    }
-}
+    self.goToFolder = function (folder) {
+        self.chosenFolderId(folder);
+        self.chosenMailData(null); // Stop showing a mail
+        $.get(
+            '/mail',
+            {
+                folder: folder
+            },
+            self.chosenFolderData);
+    };
+
+    self.goToMail = function (mail) {
+        self.chosenFolderId(mail.folder);
+        self.chosenFolderData(null); // Stop showing a folder
+        $.get(
+            '/mail',
+            {
+                mailId: mail.id
+            },
+            self.chosenMailData);
+    };
+
+    // Show inbox by default
+    self.goToFolder('Inbox');
+};
+
